01bb96f680da861abbd087a9c7c79c5e046ab25c
ziv <ziv921@yahoo.com>
2015-10-31 19:09:05 -0400

    creating people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,28 @@
-get '/people' do 
-	@people = Person.all
-	erb :"/people/index"
+
+get '/people' do
+  @people = Person.all
+  erb :"/people/index"
+end
+
+get '/people/new' do
+  @person = Person.new
+  erb :"/people/new"
+end
+
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+  
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  redirect "/people/#{person.id}"
+end
+
+get '/people/:id' do
+  @person = Person.find(params[:id])
+  birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
+  @message = Person.get_message(birth_path_num)  
+  erb :"/people/show"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 require 'faker'
- 
-Person.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, birthdate: Faker::Date.between(80.years.ago, 18.years.ago))
+(1..40).each do |i|
+  Person.create(first_name: Faker::Name.first_name, last_name: Faker::Name.first_name, birthdate: Faker::Date.between(80.years.ago, 18.years.ago))
+end
